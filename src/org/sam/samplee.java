2f6aafd117add6368759e73d5fee64e9980275b0
riyasri1166 <riyasri1166@gmail.com>
2023-09-27 12:42:28 +0530

    Updated project

---
+++ src/org/sam/samplee.java
@@ -0,0 +1,10 @@
+package org.sam;
+
+public class samplee {
+
+	public static void main(String[] args) {
+		System.out.println("Test1,Jeevi oru Korangu");
+		System.out.println("akka oru teachermadam");
+		System.out.println("priya oru charming girl");
+	}
+}
